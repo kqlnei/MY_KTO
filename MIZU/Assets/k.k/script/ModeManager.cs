d41778c180c26b05cab91f4b32fb413be39c9f7c
kqlnei <kouami0907@icloud.com>
2024-09-17 14:50:12 +0900

    damage

---
+++ MIZU/Assets/k.k/script/ModeManager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ModeManager : MonoBehaviour
+{
+    public GameObject player1Model;   // player１モデル
+    public GameObject player2Model;   // player２モデル
+
+    KK_PlayerModelSwitcher playerMode; //呼ぶスクリプトにあだなつける
+
+    public string nowmodelTag;
+    // Start is called before the first frame update
+    void Start()
+    { 
+        GameObject obj = player1Model;
+        playerMode = obj.GetComponent<KK_PlayerModelSwitcher>(); //付いているスクリプトを取得
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        nowmodelTag = playerMode.currentModel.tag;
+        Debug.Log(playerMode.currentModel.tag);
+    }
+}
